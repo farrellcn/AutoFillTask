99ea5d6eb000f70c40e239e1784132f44e0b4d29
Farrellcn <farrellcn@gmail.com>
2017-02-28 14:33:26 +0800

    add Make.bat to scripts

--- script/Make.bat
+++ script/Make.bat
@@ -1,1 +1,4 @@
 C:\Python27\Scripts\pyinstaller ..\AutoFillTask.py
+mkdir dist\AutoFillTask\config
+copy ..\config\config.ini dist\AutoFillTask\config\ /Y
+copy ..\config\CRM.xml dist\AutoFillTask\config\ /Y
